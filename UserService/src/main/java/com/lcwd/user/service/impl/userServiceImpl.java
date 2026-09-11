4cf8cb44c452977838359bf2940aec12925d68e0
krushna-kandekar-a <krushnakandekar@autoappy.com>
2026-09-12 00:38:46 +0530

    Custom Exception Handle

---
+++ UserService/src/main/java/com/lcwd/user/service/impl/userServiceImpl.java
@@ -0,0 +1,32 @@
+package com.lcwd.user.service.impl;
+
+import com.lcwd.user.service.entities.User;
+import com.lcwd.user.service.exceptions.ResourceNotFoundException;
+import com.lcwd.user.service.repositories.UserRepository;
+import com.lcwd.user.service.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class userServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public List<User> getAllUser() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getUser(String userId) {
+        return userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id not found on server !! : "+userId));
+    }
+}
