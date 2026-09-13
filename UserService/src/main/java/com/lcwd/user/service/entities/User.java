e95432537c97818b99369c83a3df9f3fba887a2c
krushna-kandekar-a <krushnakandekar@autoappy.com>
2026-09-13 12:40:30 +0530

    User Microservice Done

--- UserService/src/main/java/com/lcwd/user/service/entities/User.java
+++ UserService/src/main/java/com/lcwd/user/service/entities/User.java
@@ -1,10 +1,16 @@
 package com.lcwd.user.service.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "micro_users")
 public class User {
@@ -21,5 +27,8 @@ public class User {
 
      @Column(name = "ABOUT")
      private String about;
+
+     @Transient
+     private List<Rating> ratings = new ArrayList<>();
 
 }
