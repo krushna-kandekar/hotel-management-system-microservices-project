b19cf5c25fd44c42ffeaa3d389d0b693df9dbc0e
krushna-kandekar-a <krushnakandekar@autoappy.com>
2026-09-22 00:03:48 +0530

    Solved PgAdmin Problem

---
+++ HotelService/src/main/java/com/lcwd/hotel/services/Impl/HotelServiceImpl.java
@@ -0,0 +1,34 @@
+package com.lcwd.hotel.services.Impl;
+
+import com.lcwd.hotel.entities.Hotel;
+import com.lcwd.hotel.exceptions.ResourceNotFoundException;
+import com.lcwd.hotel.repositories.HotelRepo;
+import com.lcwd.hotel.services.HotelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class HotelServiceImpl implements HotelService {
+
+    @Autowired
+    private HotelRepo hotelRepo;
+
+    @Override
+    public Hotel create(Hotel hotel) {
+        return hotelRepo.save(hotel);
+    }
+
+    @Override
+    public List<Hotel> getAll() {
+        return hotelRepo.findAll();
+    }
+
+    @Override
+    public Hotel get(String id) {
+        return hotelRepo.findById(id).orElseThrow(()-> new ResourceNotFoundException("Hotel with given id not found !!"));
+    }
+}
+
