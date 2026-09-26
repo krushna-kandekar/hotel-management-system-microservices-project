4492755dcdcf1b210b4ce291bb58ceae9ada19e5
krushna-kandekar-a <krushnakandekar@autoappy.com>
2026-09-26 22:04:06 +0530

    Rating Service Updated

--- RatingService/src/main/java/com/lcwd/rating/services/Impl/RatingServiceImpl.java
+++ RatingService/src/main/java/com/lcwd/rating/services/Impl/RatingServiceImpl.java
@@ -1,30 +1,37 @@
 package com.lcwd.rating.services.Impl;
 
 import com.lcwd.rating.entities.Rating;
+import com.lcwd.rating.repository.RatingRepository;
 import com.lcwd.rating.services.RatingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class RatingServiceImpl implements RatingService {
+
+    @Autowired
+    private RatingRepository ratingRepository;
+
     @Override
     public Rating create(Rating rating) {
-        return null;
+        return ratingRepository.save(rating);
     }
 
     @Override
     public List<Rating> getRatings() {
-        return List.of();
+        return ratingRepository.findAll();
     }
 
     @Override
     public List<Rating> getRatingByUserId(String userId) {
-        return List.of();
+        return ratingRepository.findByUserId(userId);
     }
 
     @Override
     public List<Rating> getRatingByHotelId(String hotelId) {
-        return List.of();
+        return ratingRepository.findByHotelId(hotelId);
     }
 }
